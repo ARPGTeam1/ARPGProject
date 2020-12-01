48b76a91b4440b393303da5ab2904d2da0ae7b8c
David Engblom <kirellen1573@zoho.eu>
2020-12-01 14:35:20 +0100

    Added quit button w/ menu

---
+++ ARPGProject/Assets/Scripts/Menu/ToggleMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public class ToggleMenu : MonoBehaviour
+    {
+        public GameObject menu;
+        public KeyCode toggleKey;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(this.toggleKey))
+                Toggle();
+        }
+
+        private void Toggle()
+        {
+            this.menu.SetActive(!this.menu.activeInHierarchy);
+        }
+
+        public void QuitGame()
+        {
+            #if UNITY_STANDALONE
+            Application.Quit();
+            #endif
+            
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+}
