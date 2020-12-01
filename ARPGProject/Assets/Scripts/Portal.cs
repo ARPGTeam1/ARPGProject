6049b971f470b22d6fe4528ac695fc3d54cb7e9e
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-01 17:13:31 +0100

    Adds Portals

---
+++ ARPGProject/Assets/Scripts/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private Portal myOtherPortal;
+    [SerializeField] private Transform spawnLocation;
+
+    private void Teleport(GameObject toTeleport)
+    {
+        var NavMesh = toTeleport.GetComponent<NavMeshAgent>();
+        NavMesh.Warp(myOtherPortal.spawnLocation.transform.position);
+        toTeleport.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //NavMesh.isStopped = true;
+
+    }
+
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player"))
+            Teleport(other.gameObject);
+    }
+}
