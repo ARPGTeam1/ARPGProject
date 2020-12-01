ad78d4a32287d2e0eef29daa3bb57c65a46fa632
David Engblom <kirellen1573@zoho.eu>
2020-12-01 16:13:45 +0100

    Added movement mechanic

---
+++ ARPGProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    public float followSpeed;
+
+    [SerializeField] private Vector3 offset;
+
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, this.player.transform.position + this.offset,
+            Time.deltaTime * this.followSpeed);
+    }
+}
---
+++ ARPGProject/Assets/Scripts/Characters/Player/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Characters.Player
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Movement : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+        private Camera _cam;
+
+        private LayerMask _ground;
+
+        private void Awake()
+        {
+            this._agent = GetComponent<NavMeshAgent>();
+            this._cam = Camera.main;
+            this._ground = LayerMask.GetMask("Ground");
+        }
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(1))
+                MoveToLocation();
+        }
+
+        private void MoveToLocation()
+        {
+            var ray = this._cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, this._cam.farClipPlane, this._ground))
+                this._agent.destination = hit.point;
+        }
+    }
+}
