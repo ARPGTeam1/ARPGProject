727cbb868aeb8f5b76089130c0b6f2b1371b1b33
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-03 09:51:02 +0100

    Portal and Level change prefab

---
+++ ARPGProject/Assets/Scripts/PortalToScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalToScene : MonoBehaviour
+{
+    public Object scene;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene(scene.name);
+        
+    }
+}
