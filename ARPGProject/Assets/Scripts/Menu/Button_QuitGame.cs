40e956031e36f8246ceac74aeb0da54f9fb0b4b5
Oskar Jansson <oskar_slim89@hotmail.com>
2020-12-07 19:05:12 +0100

    more main menu stuffs

---
+++ ARPGProject/Assets/Scripts/Menu/Button_QuitGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void Quit()
+    {
+
+    #if UNITY_STANDALONE
+    Application.Quit();
+    #endif
+            
+    #if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        
+    }
+
+}
