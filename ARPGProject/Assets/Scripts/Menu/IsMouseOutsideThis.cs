50ebed654ee982c8dee30d3166061883b81cda8d
Oskar Jansson <oskar_slim89@hotmail.com>
2020-12-07 19:05:00 +0100

    Main menu stuffs

---
+++ ARPGProject/Assets/Scripts/Menu/IsMouseOutsideThis.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class IsMouseOutsideThis : MonoBehaviour
+{
+    
+
+    
+    private GraphicRaycaster raycast;
+    public bool IsPointerOutside()
+    {
+ 
+            PointerEventData pointerData = new PointerEventData (EventSystem.current)
+            {
+                pointerId = -1,
+            };
+         
+            pointerData.position = Input.mousePosition;
+ 
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.IsPointerOverGameObject();
+
+            Debug.Log( results.Count );
+        
+        
+            
+        if (EventSystem.current.currentSelectedGameObject != this.gameObject)
+        {
+            
+            return true;
+        }
+        return false;
+    }
+    
+}
+
+
---
+++ ARPGProject/Assets/Scripts/Menu/UI_Open_Anim.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Open_Anim : MonoBehaviour
+{
+    private float scale;
+    private float scaler = 0.8f;
+    bool done = false;
+
+    
+    private bool render => FramesDelay <= 0;
+    public int FramesDelay = 0;
+
+    void Start()
+    {
+        this.GetComponent<Image>().enabled = false;
+
+    }
+
+    void FixedUpdate()
+    {
+        this.GetComponent<Image>().enabled = render;
+        FramesDelay--;
+        if (FramesDelay <= 0) 
+        {
+            ScaleUp();
+        } 
+        
+    }
+
+
+    public void ScaleUp()
+    {
+        transform.localScale = new Vector3(scale, scale, scale);
+        if (!done)
+        {
+            scale = 1-scaler;
+            scaler *= 0.9f;
+            
+            if (scaler < 0.001f) scaler = 0.000f;
+            if (scale >= 1)
+            {
+                scale = 1;
+                done = true;
+                this.enabled = false;
+            }
+            
+        }
+    }
+
+
+}
