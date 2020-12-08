04e5c7b0444afd408e67204fa46c04f55b70d304
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-08 19:13:58 +0100

    Adds SoundTrigger
    it remembers if the clip with this name has been played before, if so, it doesnt play again

---
+++ ARPGProject/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip clip;
+
+    public bool played {
+        get => PlayerPrefs.GetInt(clip.name, 0) != 0;
+        set => PlayerPrefs.SetInt(clip.name, value ? 1 : 0);
+    }
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (played || !other.gameObject.CompareTag("Player")) return;
+        _audioSource.clip = clip;
+        _audioSource.Play();
+        played = true;
+    }
+    
+    [ContextMenu("Clear Played Audio Queues")]
+    public void ClearSongsPlayed()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
