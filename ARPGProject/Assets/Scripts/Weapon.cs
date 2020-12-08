b9582623a348ad2f21979c1a3d23a3ef5f293b86
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-08 23:27:15 +0100

    adds Weapon ScriptableObjects + Weapons script

---
+++ ARPGProject/Assets/Scripts/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour, IPlayerDamage, ICollectable, IEquipable
+{
+    [SerializeField] private WeaponStats stats;
+
+    public int GetDamage() => stats.damage;
+
+    public void Equip()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Collect()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public override string ToString() => stats.name;
+}
+
+public interface IEquipable
+{
+    void Equip();
+}
+
+public interface IPlayerDamage
+{
+    int GetDamage();
+}
---
+++ ARPGProject/Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon Stats", menuName = "Weapon/Weapon Stats")]
+public class WeaponStats : ScriptableObject
+{
+    //public Image icon;
+    public string weaponName;
+    public int damage;
+    public GameObject model;
+    public AudioClip hitSound;
+}
