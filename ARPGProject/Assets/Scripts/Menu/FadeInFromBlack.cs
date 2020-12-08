29f4425ccd791b9a7030c47737f8eb12afaa4a36
Oskar Jansson <oskar_slim89@hotmail.com>
2020-12-08 15:01:30 +0100

    Polished up Main Menu UI stuffs. Play button now switches scene to Scene 1.

---
+++ ARPGProject/Assets/Scripts/Menu/FadeInFromBlack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInFromBlack : MonoBehaviour
+{
+        
+    private void Start()
+    {
+        var image = GetComponent<Image>();
+        var tempcolor = image.color;
+        tempcolor = new Color(0,0,0,1);
+        image.color = tempcolor;
+        
+        
+        var rect = GetComponent<RectTransform>();
+    
+        rect.offsetMax = Vector2.zero;
+        rect.offsetMin = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        var image = GetComponent<Image>();
+        var tempcolor = image.color;
+        
+        tempcolor.a -= 0.05f;
+        image.color = tempcolor;
+
+        if (tempcolor.a <= 0)
+        {
+            DestroyImmediate(this);
+        }
+    }
+}
+
---
+++ ARPGProject/Assets/Scripts/Menu/MainMenuBackgroundFade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuBackgroundFade : MonoBehaviour
+{
+
+    public BlackScreenFadeIn blackscreen;
+    public bool fadein = false;
+    private float scaler = 1f;
+    private bool fadeDone = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {  
+        var image = GetComponent<Image>();
+        var tempcolor = image.color;
+        
+        
+        if (fadein)
+        {
+            tempcolor.a += 0.05f;
+            image.color = tempcolor;
+
+            if (tempcolor.a >= 1f)
+            {
+                fadein = false;
+                fadeDone = true;
+                blackscreen.fadein = true;
+            }
+        }
+
+        if (fadeDone)
+        {
+            this.transform.localScale *= scaler;
+
+            scaler *= 1.00001f;
+        }
+        
+    }
+    
+}
