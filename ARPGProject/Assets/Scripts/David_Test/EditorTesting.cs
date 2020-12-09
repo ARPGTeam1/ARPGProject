2d7f8095394bec15a775aac3c6d7ed121c934e5a
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-10 00:55:24 +0100

    Hope gained
    +1 Morale

---
+++ ARPGProject/Assets/Scripts/David_Test/EditorTesting.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace David_Test
+{
+    public class EditorTesting : EditorWindow
+    {
+        [MenuItem("Whispers of Hope/You're Breathtaking!!")]
+        public static void BreathTaking()
+        {
+            
+        }
+    }
+}
