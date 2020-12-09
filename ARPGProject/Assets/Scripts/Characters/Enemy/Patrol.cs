0ef1f9a51aaa8e7a934ddbd92909b180ceb9136b
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2020-12-09 11:15:57 +0100

    Clear Code in Patrol.cs

--- ARPGProject/Assets/Scripts/Characters/Enemy/Patrol.cs
+++ ARPGProject/Assets/Scripts/Characters/Enemy/Patrol.cs
@@ -11,12 +11,7 @@ namespace Characters.Enemy
         
         void Start () {
             agent = GetComponent<NavMeshAgent>();
-
-            // Disabling auto-braking allows for continuous movement
-            // between points (ie, the agent doesn't slow down as it
-            // approaches a destination point).
             agent.autoBraking = false;
-
             GotoNextPoint();
         }
         
@@ -25,11 +20,9 @@ namespace Characters.Enemy
                 return;
 
             agent.destination = points[destPoint].position;
-            
             destPoint = (destPoint + 1) % points.Length;
         }
-
-
+        
         void Update () {
             if (!agent.pathPending && agent.remainingDistance < 0.5f)
                 GotoNextPoint();
