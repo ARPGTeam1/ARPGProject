30a2c7659d11288998ea0e8e50a802c20cc65a7f
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2020-12-09 15:39:46 +0100

    Added Sound Trigger Ambience.cs

---
+++ ARPGProject/Assets/Scripts/SoundTriggerAmbience.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource), typeof(BoxCollider))]
+public class SoundTriggerAmbience : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip clip;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player") || _audioSource.isPlaying) return;
+        PlayClip();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _audioSource.Stop();
+    }
+
+    private void PlayClip()
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+
+}
