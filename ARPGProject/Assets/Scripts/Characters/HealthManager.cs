a78587ddc706ed49bee2e244bd064efd7437d789
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-14 23:49:29 +0100

    FixesLightRounding

--- ARPGProject/Assets/Scripts/Characters/HealthManager.cs
+++ ARPGProject/Assets/Scripts/Characters/HealthManager.cs
@@ -9,109 +9,109 @@ namespace Characters
 {
     public class HealthManager: MonoBehaviour, IDamagable, IKillable
     {
-    [SerializeField] private bool spawnDamageText = false;
-    [SerializeField] private GameObject damageTextPrefab;
-    [HideInInspector] public UnityEvent<int> HPChanged;
-    [HideInInspector] public UnityEvent<string> BeKilledBy;
-    public event Action OnHealthChanged;
-    public event Action<int> OnDamaged;
-    public event Action OnDeath;
-    public UnityEvent onDeath;
-    private Animator _animator;
-    private AudioSource _source;
-    public AudioClip deathSound;
-    
-    private int _currentHealth;
-    
-    [SerializeField] private int maxHealth;
+        [SerializeField] private bool spawnDamageText = false;
+        [SerializeField] private GameObject damageTextPrefab;
+        [HideInInspector] public UnityEvent<int> HPChanged;
+        [HideInInspector] public UnityEvent<string> BeKilledBy;
+        public event Action OnHealthChanged;
+        public event Action<int> OnDamaged;
+        public event Action OnDeath;
+        public UnityEvent onDeath;
+        private Animator _animator;
+        private AudioSource _source;
+        public AudioClip deathSound;
+        
+        private int _currentHealth;
+        
+        [SerializeField] private int maxHealth;
 
-    public int CurrentHealth
-    {
-        get => _currentHealth;
-        private set
+        public int CurrentHealth
         {
-            var clampValue = Mathf.Clamp(value, 0, MaxHealth);
-            _currentHealth = clampValue;
-            HPChanged.Invoke(_currentHealth);
-        }
-    }
-    
-    public int MaxHealth
-    {
-        get => maxHealth;
-        private set => maxHealth = value;
-    }
-    
-    public bool IsDead => CurrentHealth <= 0;
-
-    private void Start()
-    {
-        CurrentHealth = MaxHealth;
-        this._animator = GetComponentInChildren<Animator>();
-        this._source = GetComponent<AudioSource>();
-    }
-
-    public void TakeDamage(int damage, string source)
-    {
-        if (IsDead) return;
-
-        OnHealthChanged?.Invoke();
-        OnDamaged?.Invoke(damage);
-        if (CompareTag("Player"))
-        {
-            //Animation and Sound for Player on Damaging;
+            get => _currentHealth;
+            private set
+            {
+                var clampValue = Mathf.Clamp(value, 0, MaxHealth);
+                _currentHealth = clampValue;
+                HPChanged.Invoke(_currentHealth);
+            }
         }
         
-        if (CompareTag("Enemy"))
+        public int MaxHealth
         {
-            _animator.SetTrigger("GetHit");
+            get => maxHealth;
+            private set => maxHealth = value;
+        }
+        
+        public bool IsDead => CurrentHealth <= 0;
+
+        private void Start()
+        {
+            CurrentHealth = MaxHealth;
+            this._animator = GetComponentInChildren<Animator>();
+            this._source = GetComponent<AudioSource>();
         }
 
-        if (spawnDamageText) DamageText(damage);
-        CurrentHealth -= damage;
-        if (IsDead)
+        public void TakeDamage(int damage, string source)
         {
-            onDeath.Invoke();
-            BeKilledBy.Invoke($"You are Defeated by {source}");
-        }
-    }
+            if (IsDead) return;
 
-    public void Kill()
-    {
-        OnDeath?.Invoke();
-        if (CompareTag("Player"))
+            OnHealthChanged?.Invoke();
+            OnDamaged?.Invoke(damage);
+            if (CompareTag("Player"))
+            {
+                //Animation and Sound for Player on Damaging;
+            }
+            
+            if (CompareTag("Enemy"))
+            {
+                _animator.SetTrigger("GetHit");
+            }
+
+            if (spawnDamageText) DamageText(damage);
+            CurrentHealth -= damage;
+            if (IsDead)
+            {
+                onDeath.Invoke();
+                BeKilledBy.Invoke($"You are Defeated by {source}");
+            }
+        }
+
+        public void Kill()
         {
-            _animator.SetBool("Dead", true);
-            _source.PlayOneShot(deathSound);
+            OnDeath?.Invoke();
+            if (CompareTag("Player"))
+            {
+                _animator.SetBool("Dead", true);
+                _source.PlayOneShot(deathSound);
+            }
+
+            if (CompareTag("Enemy"))
+            {
+                _animator.SetTrigger("Die");
+                Destroy(gameObject, 1);
+            }
         }
 
-        if (CompareTag("Enemy"))
+        public void Heal()
         {
-            _animator.SetTrigger("Die");
-            Destroy(gameObject, 1);
+            CurrentHealth = MaxHealth;
+            OnHealthChanged?.Invoke();
         }
-    }
 
-    public void Heal()
-    {
-        CurrentHealth = MaxHealth;
-        OnHealthChanged?.Invoke();
-    }
+        public void Heal(int amount)
+        {
+            CurrentHealth += amount;
+            OnHealthChanged?.Invoke();
+        }
 
-    public void Heal(int amount)
-    {
-        CurrentHealth += amount;
-        OnHealthChanged?.Invoke();
-    }
+        public void DamageText(int damage)
+        {
+            Instantiate(damageTextPrefab).GetComponent<DamageText>().Setup(transform, damage);
+        }
 
-    public void DamageText(int damage)
-    {
-        Instantiate(damageTextPrefab).GetComponent<DamageText>().Setup(transform, damage);
-    }
-
-    public float HealthFillRatio()
-    {
-        return CurrentHealth / MaxHealth;
-    }
+        public float HealthFillRatio()
+        {
+            return (float)CurrentHealth / MaxHealth;
+        }
     }
 }
