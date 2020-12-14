5532adfe7d9e61f5d1bcc51342a42f9dc6b72418
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2020-12-14 21:58:51 +0100

    Add HealthManager.cs to instead HP & Health. As well as HealthBar.cs

--- ARPGProject/Assets/Scripts/Characters/HealthBar.cs
+++ ARPGProject/Assets/Scripts/Characters/HealthBar.cs
@@ -1,29 +1,28 @@
-using System;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace Characters.Enemy
+namespace Characters
 {
-
     public class HealthBar : MonoBehaviour
     {
         private static Image HPBarImage;
         public UnityEvent<float> BarValueChanged;
         public UnityEvent<Color> BarColorChanged;
         public UnityEvent<string> BarTextChanged;
-        private Health hitPoint;
+        private HealthManager hitPoint;
+
         private void Start()
         {
-            hitPoint = GetComponentInParent<Health>();
+            hitPoint = GetComponentInParent<HealthManager>();
             HPBarImage = GetComponent<Image>();
-            this.hitPoint.HPChanged.AddListener(OnHPChanged);
+            hitPoint.HPChanged.AddListener(OnHPChanged);
             OnHPChanged(hitPoint.MaxHealth);
         }
 
         private void OnHPChanged(int value)
         {
-            var barValue = (float)value / hitPoint.MaxHealth;
+            var barValue = (float) value / hitPoint.MaxHealth;
             BarValueChanged.Invoke(barValue);
             BarColorChanged.Invoke(SetHPBarValue(barValue));
             BarTextChanged.Invoke($"{value} / {hitPoint.MaxHealth}");
@@ -31,24 +30,17 @@ namespace Characters.Enemy
 
         public static Color SetHPBarValue(float value)
         {
-            if(value < 0.2f)
-            {
+            if (value < 0.2f)
                 return Color.red;
-            }
-            else if(value < 0.4f)
-            {
+            if (value < 0.4f)
                 return Color.yellow;
-            }
-            else
-            {
-                return Color.green;
-            }
+            return Color.green;
         }
 
         private void LateUpdate()
         {
             transform.LookAt(Camera.main.transform);
-            transform.Rotate(0,180,0);
+            transform.Rotate(0, 180, 0);
         }
     }
 }
---
+++ ARPGProject/Assets/Scripts/Characters/HealthManager.cs
@@ -0,0 +1,117 @@
+using System;
+using Interfaces;
+using Menu;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+namespace Characters
+{
+    public class HealthManager: MonoBehaviour, IDamagable, IKillable
+    {
+    [SerializeField] private bool spawnDamageText = false;
+    [SerializeField] private GameObject damageTextPrefab;
+    [HideInInspector] public UnityEvent<int> HPChanged;
+    [HideInInspector] public UnityEvent<string> BeKilledBy;
+    public event Action OnHealthChanged;
+    public event Action<int> OnDamaged;
+    public event Action OnDeath;
+    public UnityEvent onDeath;
+    private Animator _animator;
+    private AudioSource _source;
+    public AudioClip deathSound;
+    
+    private int _currentHealth;
+    
+    [SerializeField] private int maxHealth;
+
+    public int CurrentHealth
+    {
+        get => _currentHealth;
+        private set
+        {
+            var clampValue = Mathf.Clamp(value, 0, MaxHealth);
+            _currentHealth = clampValue;
+            HPChanged.Invoke(_currentHealth);
+        }
+    }
+    
+    public int MaxHealth
+    {
+        get => maxHealth;
+        private set => maxHealth = value;
+    }
+    
+    public bool IsDead => CurrentHealth <= 0;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+        this._animator = GetComponentInChildren<Animator>();
+        this._source = GetComponent<AudioSource>();
+    }
+
+    public void TakeDamage(int damage, string source)
+    {
+        if (IsDead) return;
+
+        OnHealthChanged?.Invoke();
+        OnDamaged?.Invoke(damage);
+        if (CompareTag("Player"))
+        {
+            //Animation and Sound for Player on Damaging;
+        }
+        
+        if (CompareTag("Enemy"))
+        {
+            _animator.SetTrigger("GetHit");
+        }
+
+        if (spawnDamageText) DamageText(damage);
+        CurrentHealth -= damage;
+        if (IsDead)
+        {
+            onDeath.Invoke();
+            BeKilledBy.Invoke($"You are Defeated by {source}");
+        }
+    }
+
+    public void Kill()
+    {
+        OnDeath?.Invoke();
+        if (CompareTag("Player"))
+        {
+            _animator.SetBool("Dead", true);
+            _source.PlayOneShot(deathSound);
+        }
+
+        if (CompareTag("Enemy"))
+        {
+            _animator.SetTrigger("Die");
+            Destroy(gameObject, 1);
+        }
+    }
+
+    public void Heal()
+    {
+        CurrentHealth = MaxHealth;
+        OnHealthChanged?.Invoke();
+    }
+
+    public void Heal(int amount)
+    {
+        CurrentHealth += amount;
+        OnHealthChanged?.Invoke();
+    }
+
+    public void DamageText(int damage)
+    {
+        Instantiate(damageTextPrefab).GetComponent<DamageText>().Setup(transform, damage);
+    }
+
+    public float HealthFillRatio()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+    }
+}
