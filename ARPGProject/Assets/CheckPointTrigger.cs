9ed16c83f91b69ab2de68b9623711ac0d01c8544
Michael Van Der Poll <michaelvdp@gmail.com>
2020-12-16 20:56:17 +0100

    Farmers Land Sound Updated

---
+++ ARPGProject/Assets/CheckPointTrigger.cs
@@ -0,0 +1,10 @@
+﻿using Characters.Player;
+using UnityEngine;
+
+public class CheckPointTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<Revive>().checkPoint = transform.position;
+    }
+}
--- ARPGProject/Assets/Scripts/HealthPickup.cs
+++ ARPGProject/Assets/Scripts/HealthPickup.cs
@@ -9,6 +9,7 @@ public class HealthPickup : MonoBehaviour, IConsumable
     [SerializeField] private int healAmount;
     [SerializeField] private GameObject pickUpEffect;
 
+    [FMODUnity.EventRef] [SerializeField] private string pickUpSound;
     public void Consume(Pandora pandora)
     {
         if(healAmount == 0)
@@ -17,7 +18,11 @@ public class HealthPickup : MonoBehaviour, IConsumable
             pandora.gameObject.GetComponent<HealthManager>().Heal(healAmount);
 
         if(pickUpEffect != null)
+        {
             Instantiate(pickUpEffect, transform.position, this.gameObject.transform.rotation);
+            FMODUnity.RuntimeManager.PlayOneShot(pickUpSound, transform.position);
+        }
+        
 
         Destroy(gameObject);
     }
