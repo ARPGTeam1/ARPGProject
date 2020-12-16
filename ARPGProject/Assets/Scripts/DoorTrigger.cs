6ff77a1666eacf19a74c3a14bd261f61dd90091f
OutaCash87 <37974186+OutaCash87@users.noreply.github.com>
2020-12-16 13:34:20 +0100

    animation add

---
+++ ARPGProject/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject cellarDoor;
+
+    [SerializeField] private bool isOpened = false;
+    
+
+
+    private void OnTriggerEnter(Collider col)
+    {            
+        if(isOpened == false)
+        {
+            cellarDoor.transform.position += new Vector3(0, 10, 0);
+            isOpened = true;
+
+        }
+
+        
+    }
+
+
+
+
+
+}
+ 
